d8eacc29cdfd57d32c30bb99ba0bed6c3c5f0d94
Anupam-1068 <107940692+Anupam-1068@users.noreply.github.com>
2024-09-30 12:32:00 +0300

    added react-spa/

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <Link to="/" className="navbar-brand">Fitness Gym</Link>
+    <Link to="/react-spa/" className="navbar-brand">Fitness Gym</Link>
     <div className="navbar-links">
       <Link to="/about-us">About Us</Link>
       <Link to="/contact-us">Contact Us</Link>
